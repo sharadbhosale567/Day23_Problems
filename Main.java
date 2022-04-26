489105aff15dc48e0db530928547ca7f8eca40a4
sharad <sharadbhosale567@gmail.com>
2022-04-26 16:57:21 +0530

    Welcome lambda

---
+++ Main.java
@@ -0,0 +1,7 @@
+package com.Day23Problems;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to lambda expression for user registration problem");
+    }
+}
